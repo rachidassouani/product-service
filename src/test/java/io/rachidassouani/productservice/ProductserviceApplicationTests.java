018d946e1ef7e8665d07783ae52e02fe3a715d50
rachidassouani <rachidassouani@gmail.com>
2024-07-15 17:32:27 +0100

    initial product service

---
+++ src/test/java/io/rachidassouani/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
